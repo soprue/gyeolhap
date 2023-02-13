c938b20c5abfa57990a57db0ee36f9a48d3feff6
soprue <soprue@naver.com>
2023-02-14 00:33:19 +0900

    Feat : tailwind CSS 적용

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'yellow': '#ffee58',
+      'green': '#66bb6a',
+      'blue': '#42a5f5',
+    },
+  },
+  plugins: [],
+}
