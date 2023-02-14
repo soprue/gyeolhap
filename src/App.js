cc4e7bd1e7cf0f7801d659e7bb973064f68d203f
soprue <soprue@naver.com>
2023-02-14 23:49:09 +0900

    Refactor : 다크모드 버튼 컴포넌트로 분리

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import { useEffect, useState } from 'react';
-import './App.css';
-import Theme from './components/theme';
+import ThemeButton from './components/ThemeButton';
 
 function App() {
   const [theme, setTheme] = useState("light");
-
-  const onChangeTheme = () => {
-    if (localStorage.getItem("theme") === "dark") {
-      localStorage.removeItem("theme");
-      document.documentElement.classList.remove("dark");
-      setTheme("light");
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      setTheme("dark");
-    }
-  };
 
   useEffect(() => {
     if (localStorage.getItem("theme") === "dark") {
@@ -25,42 +12,22 @@ function App() {
   }, []);
 
   return (
-    <div className="w-screen h-screen text-3xl text-blue text-center dark:bg-black">
-      <p className="dark:text-darkWhite">
-        결! 합!
+    <div className="w-screen h-screen flex flex-col justify-center items-center font-GothicA1 text-center text-black bg-[#f8f9fa] dark:bg-black">
+      <p className="text-4xl mb-5 font-bold	dark:text-dark-white">
+        결! 합! 게임
       </p>
       
-      <div className="w-56 h-56 bg-white mx-auto my-0">
+      <div className="w-104 h-128 rounded-2xl mx-auto my-0 shadow-md bg-white dark:bg-dark-white">
 
       </div>
 
-      {/* <Theme theme={theme} setTheme={setTheme} /> */}
-      <button
-        type="button"
-        className="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-        onClick={onChangeTheme}
-      >
-        <svg
-          className={`${theme === "dark" ? "hidden" : ""} h-5 w-5`}
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-        </svg>
-        <svg
-          className={`${theme === "light" ? "hidden" : ""} h-5 w-5`}
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-            fillRule="evenodd"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </button>
+      <div className="w-104 flex justify-between mt-2.5">
+        <button className="h-10 px-3.5 rounded-full shadow-md text-black bg-white dark:bg-dark-white">
+          규칙 보기
+        </button>
+
+        <ThemeButton theme={theme} setTheme={setTheme} />
+      </div>
     </div>
   );
 }
